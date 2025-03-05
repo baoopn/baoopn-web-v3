dd794d64131f2d9214d0700c44fa9ac667efe322
Bao Nguyen <pbnguyen@ualberta.ca>
2025-03-05 10:15:30 -0700

    Update API constants: define API_URL, CONTACT_FORM_ENDPOINT, and modify LISTENING_API_URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,3 @@
-export const LISTENING_API_URL = 'listening.baoopn.com'
+export const API_URL = 'https://api.baoopn.com';
+export const CONTACT_FORM_ENDPOINT = '/api/message';
+export const LISTENING_API_URL = 'api.baoopn.com/ws/spotify';
