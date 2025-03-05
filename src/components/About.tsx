980c92ad080112a02a622bd2f152d2c109147ef0
Bao Nguyen <pbnguyen@ualberta.ca>
2025-03-05 10:12:44 -0700

    Refactor About component: improve text formatting and update social link styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -28,31 +28,37 @@ const AboutSection = () => {
   return (
     <section className="min-h-screen flex items-center justify-center py-24">
       <div className="max-w-4xl m-auto flex items-center flex-col px-4">
-        <Reveal >
+        <Reveal>
           <h2 className="text-4xl font-semibold text-center">About Me</h2>
         </Reveal>
-        <Reveal width="100%" >
+        <Reveal width="100%">
           <p className="mt-8 text-justify">
-            Hi, I&apos;m Bao Nguyen, a passionate Full-Stack Web and App Developer with a drive for creating innovative
-            solutions.
-            With a solid background in modern technologies and a knack for problem-solving, I bring creativity and
-            technical expertise to every project I undertake.
+            Hi, I&apos;m Bao Nguyen, a passionate Full-Stack Web and App
+            Developer with a drive for creating innovative solutions. With a
+            solid background in modern technologies and a knack for
+            problem-solving, I bring creativity and technical expertise to every
+            project I undertake.
           </p>
         </Reveal>
-        <Reveal width="100%" >
+        <Reveal width="100%">
           <p className="mt-4 text-justify">
-            Over the years, I have developed a deep understanding of both front-end and back-end technologies.
-            From building responsive user interfaces to designing efficient databases, I strive to deliver seamless and robust applications.
-            My journey has involved working with a variety of frameworks and tools, including React, Next.js, Node.js, and more.
+            Over the years, I have developed a deep understanding of both
+            front-end and back-end technologies. From building responsive user
+            interfaces to designing efficient databases, I strive to deliver
+            seamless and robust applications. My journey has involved working
+            with a variety of frameworks and tools, including React, Next.js,
+            Node.js, and more.
           </p>
         </Reveal>
-        <Reveal width="100%" >
+        <Reveal width="100%">
           <p className="mt-4 text-justify">
-            I&apos;m constantly exploring new trends in the tech world and learning new skills to stay ahead.
-            Whether you&apos;re looking for a dynamic web application or a complex backend system, I am excited to collaborate and bring your vision to life.
+            I&apos;m constantly exploring new trends in the tech world and
+            learning new skills to stay ahead. Whether you&apos;re looking for a
+            dynamic web application or a complex backend system, I am excited to
+            collaborate and bring your vision to life.
           </p>
         </Reveal>
-        <Reveal width="100%" >
+        <Reveal width="100%">
           <p className="mt-4 text-justify">
             Let&apos;s connect and create something amazing together!
           </p>
@@ -65,7 +71,7 @@ const AboutSection = () => {
                 href={social.href}
                 target="_blank"
                 rel="noreferrer"
-                className="text-2xl text-gray-100 hover:text-gray-400"
+                className="text-2xl text-[var(--text-color)] hover:text-[var(--text-color-lighter)]"
               >
                 <social.icon className="w-8 h-8" />
                 <span className="sr-only">{social.srText}</span>
